01633713780f5f0644ba7ff46df7f919866f6d23
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-11-27 19:47:52 -0300

    feat: arquivo ts

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,5 @@ import { Negociacao } from "./models/negociacao.js";
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao);
 
-negociacao.quantidade = 1000;
 console.log(negociacao.getData);
 console.log(negociacao.getVolume);
